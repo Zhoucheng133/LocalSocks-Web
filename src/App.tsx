65f5a963642fca3ea19297cd05b08bdd414d9b4d
zhoucheng <zhoucheng133@outlook.com>
2026-08-22 00:42:15 +0800

    添加react路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,15 @@
-export default function App(){
-  return <div></div>
+import { Route, Routes } from 'react-router'
+import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Resgiter'
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  )
 }
